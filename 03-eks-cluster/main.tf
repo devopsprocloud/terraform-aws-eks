55bce4e0bfe11bf3af9274de4213385d3adcc9db
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2025-09-21 13:11:24 +0530

    terraform-aws-eks

--- 03-eks-cluster/main.tf
+++ 03-eks-cluster/main.tf
@@ -4,17 +4,17 @@ module "eks" {
 
   name               = "${var.project}-${var.environment}"
   kubernetes_version = "1.30"
-  
+
   addons = {
-    coredns                = {}
+    coredns = {}
     eks-pod-identity-agent = {
       before_compute = true
     }
-    kube-proxy             = {}
-    vpc-cni                = {
+    kube-proxy = {}
+    vpc-cni = {
       before_compute = true
     }
-    metrics-server= {}
+    metrics-server = {}
   }
 
 
@@ -25,9 +25,9 @@ module "eks" {
   vpc_id                   = data.aws_ssm_parameter.vpc_id.value
   subnet_ids               = local.private_subnet_ids
   control_plane_subnet_ids = local.private_subnet_ids
-  
+
   create_security_group = false
-  security_group_id = local.cluster_sg_id
+  security_group_id     = local.cluster_sg_id
 
   create_node_security_group = false
   node_security_group_id     = local.node_sg_id
@@ -37,29 +37,30 @@ module "eks" {
     blue = {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["m5.large"]
-      min_size      = 2
-      max_size      = 10
-      desired_size  = 2
-      capacity_type = "SPOT"
-      
+      min_size       = 2
+      max_size       = 10
+      desired_size   = 2
+      capacity_type  = "SPOT"
+
       iam_role_additional_policies = {
-        AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+        AmazonEBS     = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
         AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
-        AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-    }
-    green = {
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.large"]
-      min_size      = 2
-      max_size      = 10
-      desired_size  = 2
-      capacity_type = "SPOT"
-      
-      iam_role_additional_policies = {
-        AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-        AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
-        AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        AmazonEFS     = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+      }
+      green = {
+        ami_type       = "AL2023_x86_64_STANDARD"
+        instance_types = ["m5.large"]
+        min_size       = 2
+        max_size       = 10
+        desired_size   = 2
+        capacity_type  = "SPOT"
+
+        iam_role_additional_policies = {
+          AmazonEBS     = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+          AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+          AmazonEFS     = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        }
+      }
     }
   }
-}
 }
--- 03-eks-cluster/provider.tf
+++ 03-eks-cluster/provider.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 6.9.0, < 7.0.0"
     }
   }
 
-  backend "s3"{
-  bucket = "devopsprocloud-remote-state"
-  key = "roboshop-eks"
-  region = "us-east-1"
-  dynamodb_table = "devopsprocloud-remote-state-lock"
-}
+  backend "s3" {
+    bucket         = "devopsprocloud-remote-state"
+    key            = "roboshop-eks"
+    region         = "us-east-1"
+    dynamodb_table = "devopsprocloud-remote-state-lock"
+  }
 }
 
 provider "aws" {
